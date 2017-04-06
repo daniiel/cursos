f4f20f9f77ab6616d357c0fb6726cbe33baa6c0e
Daniel <dabuitragoca@gmail.com>
2017-04-05 19:48:40 -0500

    curso PL/SQL avanzado
    Curso de PL/SQL avanzado con pdfs originales y documento de notas
    'ora11g - training'

---
+++ Oracle db 11g advanced PLSQL/ora11g - training.sql
@@ -0,0 +1,130 @@
+-- ---------------------------------------------
+--  FUNCTION
+-- ---------------------------------------------
+
+DECLARE
+  v_name VARCHAR2(25);
+BEGIN
+  SELECT cust_last_name INTO v_name
+  FROM  customers WHERE  customer_id = 112; 
+  dbms_output.put_line(v_name);
+END;
+/
+SELECT cust_first_name
+FROM  customer
+WHERE  cust_last_name = v_name;
+/
+-- ---------------------------------
+set serveroutput on
+VAR b_name varchar2(25);
+
+DECLARE
+  v_name VARCHAR2(25);
+BEGIN
+  SELECT cust_last_name INTO :b_name
+  FROM  customers WHERE  customer_id = 112; 
+  --dbms_output.put_line(v_name);
+END;
+/
+SELECT cust_first_name
+FROM  customers
+WHERE  cust_last_name = :b_name;
+
+
+-- ---------------------------------------------
+--  PACKAGE
+-- ---------------------------------------------
+
+CREATE OR REPLACE PACKAGE get_cust_pkg IS
+  PROCEDURE get_customer (p_id number);
+  PROCEDURE get_customer (p_id VARCHAR2);
+END get_cust_pkg;
+/
+
+CREATE OR REPLACE PACKAGE BODY get_cust_pkg IS
+  --solucion para el problema de invocar una funcion definida despues de la invocacion
+  FUNCTION get_id (p1 varchar2) 
+    RETURN boolean;
+
+  PROCEDURE get_customer (p_id number) IS
+    v_name varchar2(25);
+  BEGIN
+    SELECT Cust_Last_Name INTO  v_name 
+    FROM customers
+    WHERE customer_id = p_id;
+    dbms_output.put_line(v_name);
+  END;
+  
+  PROCEDURE get_customer (p_id varchar2) IS
+    v_name varchar2(25);
+  BEGIN
+   	IF get_id(p_id) THEN
+    	SELECT Cust_Last_Name INTO  v_name 
+      FROM customers
+      WHERE cust_first_name = p_id;
+      dbms_output.put_line(v_name);
+    ELSE
+      dbms_output.put_line('invalid emp name');
+    END IF;
+  EXCEPTION
+    WHEN too_many_rows THEN
+      raise_application_error(-20000, 'Search by customer id instead');
+      --dbms_output.put_line('Search by customer id instead');
+  END;
+  
+  -- private subprogram
+  FUNCTION get_id (p1 varchar2) 
+    RETURN boolean IS
+    v_empid number;
+  BEGIN
+    SELECT customer_id INTO v_empid
+    FROM customers WHERE cust_first_name = p1;
+    RETURN true;
+  EXCEPTION
+    WHEN no_data_found THEN
+      RETURN false;
+  END;
+  
+END get_cust_pkg;
+/
+show errors
+
+exec Get_Cust_Pkg.get_customer(112);
+exec Get_Cust_Pkg.get_customer('Eddie');
+
+-- ---------------------------------------------
+--  CURSORES
+-- ---------------------------------------------
+
+-- opcion 1
+DECLARE
+  CURSOR empCursor IS SELECT * FROM employees 
+    WHERE department_id = 90;
+  rec_emp employees%ROWTYPE;
+BEGIN
+  OPEN empCursor;
+  LOOP
+  FETCH empCursor INTO rec_emp;
+    EXIT WHEN empCursor%NOTFOUND OR empCursor%ROWCOUNT > 4; --explicit cursor attribute
+    dbms_output.put_line(rec_emp.last_name);
+  END LOOP;
+  CLOSE empCursor;
+END;
+
+-- opcion 2
+DECLARE
+  CURSOR empCursor IS SELECT * FROM employees 
+    WHERE department_id = 90;
+BEGIN
+  FOR emprec IN empCursor LOOP
+    dbms_output.put_line(emprec.last_name);
+  END LOOP;
+END;
+
+-- opcion 3
+BEGIN
+  FOR emprec IN (SELECT * FROM employees WHERE department_id = 90) LOOP
+    dbms_output.put_line(emprec.last_name);
+  END LOOP;
+END;
+
