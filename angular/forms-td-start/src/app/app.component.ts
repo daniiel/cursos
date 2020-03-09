2c05b4e486a6bd192d8aebc3a643093249590cb6
Daniel Buitrago <daniel.buitrago@wnco.com>
2020-03-09 18:27:51 -0500

    Adding validations to fields

--- angular/forms-td-start/src/app/app.component.ts
+++ angular/forms-td-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f', {static: false}) signUpForm: NgForm;
+  // Do the same that the @viewChild statement
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  onSubmit() {
+    console.log(this.signUpForm);
+  }
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
